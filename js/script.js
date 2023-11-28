eedeba2f37211b1ac64db23e31706bd4108b37de
Lucas-Salomao <lucastadeusalomao@gmail.com>
2023-11-27 21:50:49 -0300

    update

--- js/script.js
+++ js/script.js
@@ -1,5 +1,20 @@
 const darkModeToggle = document.getElementById('dark-mode-toggle');
+let mode = "light";
 
 darkModeToggle.addEventListener('click', () => {
-  document.body.classList.toggle('dark-mode');
+
+  if (mode == "light") {
+    document.body.classList.toggle('dark-mode');
+    darkModeToggle.querySelector('i').setAttribute('class', 'fa fa-sun');
+    mode = "dark";
+  }
+  else {
+    if (mode == "dark") {
+      document.body.classList.toggle('dark-mode');
+      darkModeToggle.querySelector('i').setAttribute('class', 'fa fa-moon');
+      mode = "light";
+    }
+  }
+
+
 });
