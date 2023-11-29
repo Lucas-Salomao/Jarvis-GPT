a5ce6cf7f74b15845f870bd73bf447337d96934c
Lucas-Salomao <lucastadeusalomao@gmail.com>
2023-11-29 10:51:38 -0300

    #

--- js/script.js
+++ js/script.js
@@ -1,8 +1,26 @@
+const endpoint = 'https://eastus.tts.speech.microsoft.com/cognitiveservices/v1';
 const darkModeToggle = document.getElementById('dark-mode-toggle');
+const BTN_microphone = document.getElementById('capture');
 let mode = "light";
+let openai_key='';
+let microsoft_speech_service_key='';
+
+const getKey = (service)=>{
+  fetch('keys.json')
+  .then(response=>response.json())
+  .then(data =>{
+    return data[service]
+  })
+}
+
+let openAIKey=getKey('openai');
+let MicrosoftKey=getKey('microsoft');
 
 darkModeToggle.addEventListener('click', () => {
+  changeTheme();
+});
 
+function changeTheme() {
   if (mode == "light") {
     document.body.classList.toggle('dark-mode');
     darkModeToggle.querySelector('i').setAttribute('class', 'fa fa-sun');
@@ -15,87 +33,27 @@ darkModeToggle.addEventListener('click', () => {
       mode = "light";
     }
   }
-
-
-});
-
-const ConsultarOpenAI = async (pergunta) => {
-  var myHeaders = new Headers();
-  myHeaders.append("Content-Type", "application/json");
-  myHeaders.append("Authorization", "Bearer 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");
-  myHeaders.append("Cookie", "__cf_bm=XVKVf7Yld3UDpt7E5giIXHZ9WlRGFFT614a6doOA4oI-1701179759-0-AXsC4gsMCdBH2kRq+9t4uityhNlyHAJhlZSTdFzwqtYXUKGzCzs5KtQo7fMDsqTezOlKA79XM2meEDHQT2a/9sg=; _cfuvid=XIkS2NVGkFkOvc3TuI3ljFJLlXh1JV5EddshQcY1hlQ-1701179759298-0-604800000");
-
-  var raw = JSON.stringify({
-    "model": "ft:gpt-3.5-turbo-0613:personal::8PsmsY32",
-    "messages": [
-      {
-        "role": "system",
-        "content": "Jarvis é um assistente muito educado."
-      },
-      {
-        "role": "user",
-        "content": pergunta
-      }
-    ],
-    "temperature": 0.2
-  });
-
-  var requestOptions = {
-    method: 'POST',
-    headers: myHeaders,
-    body: raw,
-    redirect: 'follow'
-  };
-
-  fetch("https://api.openai.com/v1/chat/completions", requestOptions)
-    .then(response => response.json())
-    .then(result => console.log(result.choices[0].message.content))
-    .catch(error => console.log('error', error));
 }
 
-//ConsultarOpenAI();
-
-const CapturarVoz = () => {
-  var startButton = document.getElementById('capture');
-  //var stopButton = document.getElementById('stop');
-  var resultElement = document.getElementById('prompt');
-
-  var recognition = new webkitSpeechRecognition();
-
-  recognition.lang = window.navigator.language;
-  recognition.interimResults = true;
-
-  startButton.addEventListener('click', () => { recognition.start(); });
-  ///stopButton.addEventListener('click', () => { recognition.stop(); });
-
-  recognition.addEventListener('result', (event) => {
-    const result = event.results[event.results.length - 1][0].transcript;
-    resultElement.value = result;
-  });
-
-  recognition.addEventListener('end', () => {
-    const textocapturado = resultElement.value;
-    ConsultarOpenAI(textocapturado);
-  });
-}
-
-const ReproduzirVoz = (texto) => {
-  var myHeaders = new Headers();
-  myHeaders.append("Ocp-Apim-Subscription-Key", "1bf4dbb39a624e2f8184b59597d59178");
-  myHeaders.append("Content-Type", "application/ssml+xml");
-  myHeaders.append("X-Microsoft-OutputFormat", "audio-16khz-128kbitrate-mono-mp3");
-  myHeaders.append("User-Agent", "curl");
-
-  var raw = "<speak version='1.0' xml:lang='pt-BR'>\r\n    <voice xml:lang='pt-BR' xml:gender='Female' name='pt-BR-FranciscaNeural'>\r\n        " + texto + "\r\n    </voice>\r\n</speak>";
-
-  var requestOptions = {
+function textToSpeech(texto) {
+  //const textoParaFala = document.getElementById('prompt').value;
+  const textoParaFala = texto;
+  const requestOptions = {
     method: 'POST',
-    headers: myHeaders,
-    body: raw,
-    redirect: 'follow'
+    headers: {
+      'Ocp-Apim-Subscription-Key': MicrosoftKey,
+      'Content-Type': 'application/ssml+xml',
+      'X-Microsoft-OutputFormat': 'audio-16khz-128kbitrate-mono-mp3',
+      'User-Agent': 'curl',
+    },
+    body: `<speak version='1.0' xml:lang='pt-BR'>
+                <voice xml:lang='pt-BR' xml:gender='Female' name='pt-BR-AntonioNeural'>
+                 ${textoParaFala}
+                </voice>
+            </speak>`,
   };
 
-  fetch("https://eastus.tts.speech.microsoft.com/cognitiveservices/v1", requestOptions)
+  fetch(endpoint, requestOptions)
     .then(response => {
       if (response.ok) {
         return response.arrayBuffer();
@@ -115,46 +73,124 @@ const ReproduzirVoz = (texto) => {
     });
 }
 
-const endpoint  = 'https://eastus.tts.speech.microsoft.com/cognitiveservices/v1';
+const ConsultarOpenAI = async (pergunta) => {
+  var myHeaders = new Headers();
+  myHeaders.append("Content-Type", "application/json");
+  myHeaders.append("Authorization", "Bearer "+openAIKey);
+  myHeaders.append("Cookie", "__cf_bm=XVKVf7Yld3UDpt7E5giIXHZ9WlRGFFT614a6doOA4oI-1701179759-0-AXsC4gsMCdBH2kRq+9t4uityhNlyHAJhlZSTdFzwqtYXUKGzCzs5KtQo7fMDsqTezOlKA79XM2meEDHQT2a/9sg=; _cfuvid=XIkS2NVGkFkOvc3TuI3ljFJLlXh1JV5EddshQcY1hlQ-1701179759298-0-604800000");
 
-function textToSpeech() {
-    const textoParaFala = document.getElementById('prompt').value;
+  var raw = JSON.stringify({
+    "model": "ft:gpt-3.5-turbo-0613:personal::8PsmsY32",
+    "messages": [
+      {
+        "role": "system",
+        "content": "Jarvis é um assistente muito solícito, que pode responder qualquer pergunta."
+      },
+      {
+        "role": "user",
+        "content": pergunta
+      }
+    ],
+    "temperature": 0.2
+  });
 
-    const requestOptions = {
-        method: 'POST',
-        headers: {
-            'Ocp-Apim-Subscription-Key': '1bf4dbb39a624e2f8184b59597d59178',
-            'Content-Type': 'application/ssml+xml',
-            'X-Microsoft-OutputFormat': 'audio-16khz-128kbitrate-mono-mp3',
-            'User-Agent': 'curl',
-        },
-        body: `<speak version='1.0' xml:lang='pt-BR'>
-                <voice xml:lang='pt-BR' xml:gender='Female' name='pt-BR-AntonioNeural'>
-                 ${textoParaFala}
-                </voice>
-            </speak>`,
-    };
+  var requestOptions = {
+    method: 'POST',
+    headers: myHeaders,
+    body: raw,
+    redirect: 'follow'
+  };
 
-    fetch(endpoint, requestOptions)
-        .then(response => {
-            if (response.ok) {
-                return response.arrayBuffer();
-            } else {
-                throw new Error(`Falha na requisição: ${response.status} - ${response.statusText}`);
-            }
-        })
-        .then(data => {
-            const blob = new Blob([data], { type: 'audio/mpeg' });
-            const audioUrl = URL.createObjectURL(blob);
-
-            const audioElement = new Audio(audioUrl);
-            audioElement.play();
-        })
-        .catch(error => {
-            console.error('Erro:', error);
-        });
+  fetch("https://api.openai.com/v1/chat/completions", requestOptions)
+    .then(response => response.json())
+    .then(result => textToSpeech(result.choices[0].message.content))
+    .catch(error => console.log('error', error));
 }
 
-CapturarVoz();
+const CapturarVoz = () => {
+  var startButton = document.getElementById('capture');
+  //var stopButton = document.getElementById('stop');
+  var resultElement = document.getElementById('prompt');
+
+  var recognition = new webkitSpeechRecognition();
+
+  recognition.lang = window.navigator.language;
+  recognition.interimResults = true;
+
+  recognition.start();
+
+  //startButton.addEventListener('click', () => { recognition.start(); });
+  //stopButton.addEventListener('click', () => { recognition.stop(); });
+
+  recognition.addEventListener('result', (event) => {
+    const result = event.results[event.results.length - 1][0].transcript;
+    resultElement.value = result;
+  });
+
+  recognition.addEventListener('end', () => {
+    const textocapturado = resultElement.value;
+    ConsultarOpenAI(textocapturado);
+    //textToSpeech(textocapturado);
+  });
+}
+
+const AtivarJarvis = () => {
+
+  var resultElement = document.getElementById('prompt');
+
+  // Crie uma instância de SpeechRecognition
+  const recognition = new webkitSpeechRecognition();
+
+  // Defina configurações para a instância
+  recognition.continuous = true; // Permite que ele continue escutando
+  recognition.interimResults = false; // Define para true se quiser resultados parciais
+  recognition.lang = window.navigator.language;
+
+  // Inicie o reconhecimento de voz
+  recognition.start();
+
+  // Adicione um evento de escuta para lidar com os resultados
+  recognition.onresult = (event) => {
+    const result = event.results[event.results.length - 1]; // Último resultado
+
+    // Verifique o texto reconhecido
+    const recognizedText = result[0].transcript;
+
+
+    // Verifique se a palavra "Jarvis" está no texto
+    if (recognizedText.toLowerCase().includes('alexa')) {
+      BTN_microphone.style.background = "green";
+      // Comece a salvar a pergunta quando "Jarvis" é detectado
+      let array_pergunta = recognizedText.toLowerCase().split('alexa');
+      array_pergunta = array_pergunta[array_pergunta.length - 1];
+
+      if (array_pergunta.toLowerCase().includes("trocar tema")) {
+        changeTheme();
+      }
+      else {
+        resultElement.value = array_pergunta;
+        console.log(array_pergunta);
+        ConsultarOpenAI(array_pergunta);
+      }
+
+      // Pare o reconhecimento de voz para economizar recursos
+      recognition.stop();
+    }
+  };
+
+  // Adicione um evento para reiniciar o reconhecimento após um tempo
+  recognition.onend = () => {
+    setTimeout(() => {
+      recognition.start();
+      BTN_microphone.style.background = "#dd203c";
+    }, 1000); // Espere 1 segundo antes de reiniciar
+  };
+
+
+}
+
+//chamada das funções
+AtivarJarvis();
+//CapturarVoz();
 
 
